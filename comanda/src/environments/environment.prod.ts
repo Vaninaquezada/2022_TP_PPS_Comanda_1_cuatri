8904bce2e1af8383b99c7a63f37b188701d0837f
Vanina Quezada <vaninaquezada@gmail.com>
2022-05-30 18:21:07 -0300

    Primer commit

---
+++ comanda/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'appcomanda-cae9b',
+    appId: '1:1059233143564:web:f799fa9db2bad8b8680729',
+    storageBucket: 'appcomanda-cae9b.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyD30A4Mb9HOOX6bh671rC44nSanL9HO-Gk',
+    authDomain: 'appcomanda-cae9b.firebaseapp.com',
+    messagingSenderId: '1059233143564',
+  },
+  production: true
+};
